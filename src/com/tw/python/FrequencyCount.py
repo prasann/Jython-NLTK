62158d60aa670dc957366b54a478925655dc62f9
Prasanna <prasann@thoughtworks.com>
2012-02-08 17:24:10 +0530

    Added stemming

--- src/com/tw/python/FrequencyCount.py
+++ src/com/tw/python/FrequencyCount.py
@@ -1,24 +1,33 @@
 from com.tw import FrequencyCount
 import nltk
-from nltk.probability import FreqDist
-from nltk.corpus import stopwords
+from nltk import probability
+from nltk import corpus
+from nltk import stem
 from nltk import tokenize
 
 class FrequencyCount(FrequencyCount):
     def buildFreqMap(self, text):
-        freq_dict = FreqDist()
+        freq_dict = probability.FreqDist()
         tokens = self.tokenize(text)
-        for word in self.remove_stop_words(tokens):
+        stop_words_removed = self.remove_stop_words(tokens)
+        for word in self.stem_text(stop_words_removed):
             freq_dict.inc(word.lower())
         return freq_dict
 
-    def remove_stop_words(self, word_list):
-        stop_words = stopwords.words('english')
-        return [w for w in word_list if w.lower() not in stop_words]
-
-    def tokenize(self,text):
+    def tokenize(self, text):
         all_tokens = tokenize.word_tokenize(text)
         return [w.lower() for w in all_tokens if w.isalpha()]
+
+    def remove_stop_words(self, word_list):
+        stop_words = corpus.stopwords.words('english')
+        return [w for w in word_list if w.lower() not in stop_words]
+
+    def stem_text(self, word_list):
+        after_stem = []
+        stemmer = stem.PorterStemmer()
+        for w in word_list:
+            after_stem.append(stemmer.stem(w))
+        return after_stem
 
 
   
